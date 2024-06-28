fd6cf27f2d00a87af3ef62f24626cf292efc1074
Syed Abdul Rehman <imabdul555@gmail.com>
2024-06-28 23:25:52 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae,
+        ratione, eius quos ipsa in hic minima id dolores molestiae deserunt
+        doloribus asperiores quibusdam? Est necessitatibus eaque atque magni,
+        rerum facilis tempora fugit illo iste quos tenetur nam. Velit magnam in
+        officia. Culpa placeat consectetur et at iusto quia rem temporibus.
+      </p>
+    </>
+  );
+}
+
+export default App;
